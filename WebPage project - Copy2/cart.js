c6fb9e0771c3ecf1b7ab2db6e652aa772f7f51d7
Elvis <elviseshikena@gmail.com>
2019-11-28 00:33:28 -0800

    Add cart addition process

--- WebPage project - Copy2/cart.js
+++ WebPage project - Copy2/cart.js
@@ -1,13 +1,44 @@
+let cart = {}
+let total = 0
 
-function incrementCart()
-{
-    let my_span =document.getElementById("club-counter");
-    let value = Number(my_span.innerHTML);
-    my_span.innerHTML = value + 1;
-    console.log(value);
+let feedback = document.getElementById('feedback')
+
+let initCartItem = (shoeId) => {
+  cart[shoeId] = {}
+  cart[shoeId].quantity = 0
+  cart[shoeId].price = 0
+}
+
+let incrementCart = (shoeId) => {
+  // Create cart entry if not there
+  if (shoeId in cart === false) {
+    initCartItem(shoeId);
+  }
+
+  // Increment entry
+  cart[shoeId].quantity += 1
+  cart[shoeId].price = 200
+  console.log(cart)
+
+  total = 0
+  for (let shoe in cart){
+    total += cart[shoe].quantity*cart[shoe].price
+  }
+  
+  alert(`Added ${cart[shoeId].quantity} ${shoeId}, Price $${cart[shoeId].price}`)
+  feedback.innerText = `Total: $${total}`
 }
 
 
-  <p>Cart Value:<span id="club-counter">0</span></p>
-    <button onclick="incrementCart()">Add an item to cart</button>
+
+let button = document.getElementById("shoe1")
+// Get shoes
+let shoes = document.getElementsByClassName("resp")
+console.log(shoes)
+// Add onclick listeners to them
+for (let shoe of shoes) {
+  shoe.onclick = () => {
+    incrementCart(shoe.id)
+  }
+}
 
